ade849fac99c162ab1290b86099443949a84cfab
Dan Koza <djk12587@gmail.com>
2014-08-18 14:29:33 -0500

    inital commit

---
+++ CoreDataHelper/CoreDataHelper.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataHelper.h
+//  CoreDataHelper
+//
+//  Created by Daniel Koza on 8/18/14.
+//  Copyright (c) 2014 Daniel Koza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataHelper.
+FOUNDATION_EXPORT double CoreDataHelperVersionNumber;
+
+//! Project version string for CoreDataHelper.
+FOUNDATION_EXPORT const unsigned char CoreDataHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataHelper/PublicHeader.h>
+
+
